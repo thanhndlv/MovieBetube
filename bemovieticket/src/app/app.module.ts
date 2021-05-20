e89067765770f43401962fafa598031dab16ee89
vantungo <it.tunv@gmail.com>
2021-05-20 20:38:25 +0700

    tunv -added tikect component into app.modules

--- bemovieticket/src/app/app.module.ts
+++ bemovieticket/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import {AppRoutingModule} from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TicketComponent } from './modules/home/ticket/ticket.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TicketComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
