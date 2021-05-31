24eaf1bf1425e31c32e4e6fe5807b3c42aa78d22
vantungo <it.tunv@gmail.com>
2021-05-31 14:13:51 +0700

    tunv - add booking ticket feature

--- bemovieticket/src/app/app-routing.module.ts
+++ bemovieticket/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from "./modules/home/home/home.module";
 import { AdminModule } from "./modules/admin/admin.module";
-import { AccountModule } from "./modules/home/account/account.module"
 const routes: Routes = [
   { path: "", loadChildren: "./modules/home/home/home.module#HomeModule" },
   { path: "account", loadChildren: "./modules/home/account/account.module#AccountModule" },
@@ -11,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [HomeModule, AdminModule, AccountModule, RouterModule.forRoot(routes), SweetAlert2Module.forRoot()],
+  imports: [HomeModule, AdminModule, RouterModule.forRoot(routes), SweetAlert2Module.forRoot()],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- bemovieticket/src/app/modules/home/home/home.module.ts
+++ bemovieticket/src/app/modules/home/home/home.module.ts
@@ -1,3 +1,6 @@
+import { TicketComponent } from './../ticket/ticket.component';
+import { HomeService } from './../../../_core/service/home.service';
+import { AuthInterceptor } from './../../../_core/interceptors/auth.interceptor';
 import { PipeModule } from './../../../_core/pipe/pipe.module';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -8,21 +11,26 @@ import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { SlickCarouselModule } from "ngx-slick-carousel";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 const homeRoutes: Routes = [
     {
         path: "",
         component: HomeComponent,
-        children: [{ path: "", component: IndexComponent }]
+        children: [
+            { path: "", component: IndexComponent },
+            { path: "ticket/:showTimeID", component: TicketComponent }
+        ]
     }
-    
+
 ];
 
 @NgModule({
     // declarations: [HomeComponent, IndexComponent, AboutUsComponent, ContactComponent, UtilityComponent, DetailComponent, UserDetailComponent, TicketComponent],
     // imports: [SweetAlert2Module, CommonModule, PipeModule, NgbModule, SlickCarouselModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(homeRoutes)],
     // providers: []
-    declarations: [HomeComponent, IndexComponent],
-    imports: [CommonModule, NgbModule, SlickCarouselModule, PipeModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(homeRoutes)],
-    providers: []
+    declarations: [HomeComponent, IndexComponent, TicketComponent],
+    imports: [CommonModule, NgbModule, SlickCarouselModule, PipeModule, FormsModule, SweetAlert2Module, ReactiveFormsModule, RouterModule.forChild(homeRoutes)],
+    providers: [HomeService, [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]]
 })
 export class HomeModule { }
